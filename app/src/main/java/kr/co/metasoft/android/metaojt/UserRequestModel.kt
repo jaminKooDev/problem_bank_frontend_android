d4eb764b9bab59e4e805a8153a2eedb395d84b28
jmkoo <jmkoo.dev@gmail.com>
2021-12-02 17:56:23 +0900

    사용자 API 요청 모델 추가

---
+++ app/src/main/java/kr/co/metasoft/android/metaojt/UserRequestModel.kt
@@ -0,0 +1,15 @@
+package kr.co.metasoft.android.metaojt
+
+import com.google.gson.annotations.SerializedName
+
+data class UserRequestModel(
+
+    @SerializedName("username")
+    val id: String,
+
+    @SerializedName("password")
+    val pw: String? = null,
+
+    @SerializedName("frontend")
+    val frontend: String = "frontend_user"
+)

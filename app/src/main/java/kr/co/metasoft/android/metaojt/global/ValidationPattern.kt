c6c76cea09d4a0f5f916aa7e8b44f7b5ab30f62f
jmkoo <jmkoo.dev@gmail.com>
2021-12-13 15:03:00 +0900

    유효성 검사 정규식 객체 추가

---
+++ app/src/main/java/kr/co/metasoft/android/metaojt/global/ValidationPattern.kt
@@ -0,0 +1,9 @@
+package kr.co.metasoft.android.metaojt.global
+
+object ValidationPattern {
+    const val ID_LENGTH_PATTERN = "^(?=.{8,20}$)$"
+    const val ID_ONLY_CHARACTER_AND_NUMBER_PATTERN = "[^a-zA-Z0-9._]"
+    const val PASSWORD_PATTERN = "[^a-zA-Z0-9~`!@#$%^&*()-+=_]"
+    const val PHONE_NUM_PATTERN = "^01([0|1|6|7|8|9])?([0-9]{3,4})?([0-9]{4})\$"
+    const val NAME_PATTERN = "[^a-zA-Z가-힣]"
+}

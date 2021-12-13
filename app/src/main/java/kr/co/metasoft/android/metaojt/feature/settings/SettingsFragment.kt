0746cd9d6c62c0c72f3f75e8a6e28a09b9b4827a
jmkoo <jmkoo.dev@gmail.com>
2021-12-13 14:55:17 +0900

    미사용 코드 삭제

---
+++ app/src/main/java/kr/co/metasoft/android/metaojt/feature/settings/SettingsFragment.kt
@@ -0,0 +1,17 @@
+package kr.co.metasoft.android.metaojt.feature.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SettingsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}

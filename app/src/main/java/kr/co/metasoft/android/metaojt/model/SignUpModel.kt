199f051cc4a426cfc60e7a14d80e8796754fe2f4
jmkoo <jmkoo.dev@gmail.com>
2021-12-13 15:35:07 +0900

    회원가입 api 모델 추가

---
+++ app/src/main/java/kr/co/metasoft/android/metaojt/model/SignUpModel.kt
@@ -0,0 +1,14 @@
+package kr.co.metasoft.android.metaojt.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SignUpModel(
+    @SerializedName("userDto")
+    var userDto: UserModel,
+
+    @SerializedName("personDto")
+    var personDto: PersonModel,
+
+    @SerializedName("userTypeCode")
+    val userTypeCode: Number = 2
+)

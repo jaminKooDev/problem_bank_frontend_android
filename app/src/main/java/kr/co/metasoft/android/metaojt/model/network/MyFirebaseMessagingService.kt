6e7add0641723fb22b9f2f1f897352a4dcda43b2
jmkoo <jmkoo.dev@gmail.com>
2021-12-13 15:04:56 +0900

    패키지 위치 변경

--- app/src/main/java/kr/co/metasoft/android/metaojt/model/network/MyFirebaseMessagingService.kt
+++ app/src/main/java/kr/co/metasoft/android/metaojt/model/network/MyFirebaseMessagingService.kt
@@ -1,4 +1,4 @@
-package kr.co.metasoft.android.metaojt
+package kr.co.metasoft.android.metaojt.model.network
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService

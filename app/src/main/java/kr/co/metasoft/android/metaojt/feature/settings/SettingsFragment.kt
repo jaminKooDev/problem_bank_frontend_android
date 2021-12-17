b78ba4456936a44e31b8e9885fa5505a66faf21c
jmkoo <jmkoo.dev@gmail.com>
2021-12-17 18:00:54 +0900

    SettingsFragment 초기화 작업 추가

--- app/src/main/java/kr/co/metasoft/android/metaojt/feature/settings/SettingsFragment.kt
+++ app/src/main/java/kr/co/metasoft/android/metaojt/feature/settings/SettingsFragment.kt
@@ -1,17 +1,27 @@
 package kr.co.metasoft.android.metaojt.feature.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import kr.co.metasoft.android.metaojt.R
 
-class SettingsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+class SettingsFragment :PreferenceFragmentCompat() {
+
+    lateinit var prefs : SharedPreferences
+
+    var autoLoginPreference: Preference? = null
+    var pushNotificationPreference: Preference? = null
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.basic_preferences, rootKey)
+
+        if (rootKey == null) {
+            autoLoginPreference = findPreference("auto_login")
+            pushNotificationPreference = findPreference("push_notification")
+
+        }
     }
+
+
 }

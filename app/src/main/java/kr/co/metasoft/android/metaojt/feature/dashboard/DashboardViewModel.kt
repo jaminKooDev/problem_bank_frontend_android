4f23803231624254e5c25f3fe772bc0494f7d313
jmkoo <jmkoo.dev@gmail.com>
2021-12-27 09:36:34 +0900

    context 추가 및 onBackBtnClick, logout 메서드 추가

--- app/src/main/java/kr/co/metasoft/android/metaojt/feature/dashboard/DashboardViewModel.kt
+++ app/src/main/java/kr/co/metasoft/android/metaojt/feature/dashboard/DashboardViewModel.kt
@@ -1,28 +1,50 @@
 package kr.co.metasoft.android.metaojt.feature.dashboard
 
+import android.app.Application
+import android.util.Log
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.preference.Preference
+import androidx.preference.PreferenceManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kr.co.metasoft.android.metaojt.global.Event
+import kr.co.metasoft.android.metaojt.global.Preferences
 import kr.co.metasoft.android.metaojt.model.network.ApiRepository
+import java.lang.Exception
 import kotlin.coroutines.CoroutineContext
 
 class DashboardViewModel(
-    private val repository: ApiRepository
-): ViewModel() {
+    private val repository: ApiRepository,
+    application: Application
+): AndroidViewModel(application) {
+
+    private val _context = getApplication<Application>().applicationContext
+    private val prefs = Preferences(_context)
 
     private val _navigationToLoginEvent = MutableLiveData<Event<Unit>>()
     val navigationToLoginEvent: LiveData<Event<Unit>> = _navigationToLoginEvent
+
+    private val _navigationBackEvent = MutableLiveData<Event<Unit>>()
+    val navigationBackEvent: LiveData<Event<Unit>> = _navigationBackEvent
 
     private val parentJob = Job()
     private val coroutineContext : CoroutineContext get() = parentJob + Dispatchers.Default
     private val scope = CoroutineScope(coroutineContext)
 
     fun chkTokenValidation(token: String?) {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(_context)
+        val autoLogin = sharedPreferences.getBoolean("auto_login", true)
+
         if (token == null)  _navigationToLoginEvent.value = Event(Unit)
         else {
             scope.launch {
@@ -30,6 +52,22 @@ class DashboardViewModel(
                 if (response.code() != 200) {
                     _navigationToLoginEvent.postValue(Event(Unit))
                 }
+            }
+        }
+    }
+
+    fun onBackBtnClick(view: View?) {
+        _navigationBackEvent.value = Event(Unit)
+    }
+
+    fun logout(token: String?) {
+        scope.launch {
+            try {
+                prefs.token = null
+                repository.postLogout(token!!)
+
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
     }

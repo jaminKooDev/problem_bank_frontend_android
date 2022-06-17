e124689c06966dfbf7f36f17502a10a6d1188e52
jmkoo <jmkoo.dev@gmail.com>
2022-06-17 11:05:14 +0900

    숫자만 입력 가능 패턴 추가

--- app/src/main/java/kr/co/metasoft/android/metaojt/global/ValidationPattern.kt
+++ app/src/main/java/kr/co/metasoft/android/metaojt/global/ValidationPattern.kt
@@ -6,4 +6,5 @@ object ValidationPattern {
     const val PASSWORD_PATTERN = "[^a-zA-Z0-9~`!@#$%^&*()-+=_]"
     const val PHONE_NUM_PATTERN = "^01([0|1|6|7|8|9])?([0-9]{3,4})?([0-9]{4})\$"
     const val NAME_PATTERN = "[^a-zA-Z가-힣]"
+    const val ONLY_NUMBER_PATTERN = "/^[0-9]/g"
 }
